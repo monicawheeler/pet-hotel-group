65c567c1683ce62942a2a6b2ab504529786282e1
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-01-16 14:59:02 -0600

    updated sql file with db structure. add routes and router file.

---
+++ database.sql
@@ -0,0 +1,24 @@
+
+--Tables--
+
+CREATE TABLE owners (
+	id SERIAL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(80) NOT NULL
+);
+
+CREATE TABLE pets (
+	id SERIAL PRIMARY KEY,
+	pet_name VARCHAR(80) NOT NULL,
+	breed VARCHAR(80) NOT NULL,
+	color VARCHAR(80) NOT NULL,
+	is_checked_in BOOLEAN DEFAULT true,
+	owner_id INT REFERENCES owners  
+);
+
+CREATE TABLE visits (
+	id SERIAL PRIMARY KEY,
+	check_in_date DATE DEFAULT now(),
+	check_out_date DATE,
+	pet_id INT REFERENCES pets
+);
