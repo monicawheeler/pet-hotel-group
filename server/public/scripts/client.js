61f1309cc87b687cc64bc1e1570983b50c753338
Darian N <darian.nasrabadi@gmail.com>
2018-01-16 16:30:06 -0600

    cleaned up logs and linked delete route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,7 @@ console.log('JavaScript Sourced');
 $(document).ready(function() {
    console.log('jQuery Sourced');
    $('#registerButton').on('click', addNewOwner);
+   $('#tableBody').on('click', '.deleteButton', deletePet);
    getAllPets()
 });
 
@@ -22,7 +23,6 @@ function getAllPets() {
 }
 
 function displayAllPets(data) {
-    console.log(data.is_checked_in);
     $tableRow = $('<tr>');
     $tableRow.append(`<td>${data.first_name} ${data.last_name}</td>`);
     $tableRow.append(`<td>${data.pet_name}</td>`);
@@ -31,11 +31,9 @@ function displayAllPets(data) {
     $tableRow.append(`<td><button class="editButton" value="${data.pets_id}">Edit</button></td>`);
     $tableRow.append(`<td><button class="deleteButton" value="${data.pets_id}">Delete</button></td>`);
     if (data.is_checked_in === false) {
-        console.log('in the false');
         $tableRow.append(`<td><button class="checkIn" value="${data.pets_id}">IN</button></td>`);
     }
     else if (data.is_checked_in === true) {
-        console.log('in the true');
         $tableRow.append(`<td><button class="checkOut" value="${data.pets_id}">OUT</button></td>`);
     }
     $('#tableBody').append($tableRow);
@@ -57,4 +55,22 @@ function addNewOwner() {
             //getAllPets();
         }
     });
+}
+
+function deletePet() {
+    if (confirm('Are you sure you want to remove your pet?')) {
+        let id = $(this).val()
+        console.log(id);
+        $.ajax({
+            method: 'DELETE',
+            url: '/pets/' + id,
+            success: (response)=>{
+                console.log('Inside deletePet DELETE ajax: ', response);
+                getAllPets()
+            },
+            error: ()=>{
+                alert('Error was received in deleting the pet data')
+            }
+        })
+    }
 }
