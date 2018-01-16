595c2ba6dec3c49ac247fd369c8cbb6afe2ded30
Monica Wheeler <monica.c.wheeler@gmail.com>
2018-01-16 15:49:51 -0600

    Update client.js and owners.router to POST. Insert new owners

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,26 @@
 console.log('JavaScript Sourced');
 
-
 $(document).ready(function() {
    console.log('jQuery Sourced');
+
+   // Event Listeners
+   $('#registerButton').on('click', addNewOwner);
 });
+
+function addNewOwner() {
+    const ownerToSend = {
+        first_name: $('#first_name').val(),
+        last_name: $('#last_name').val()
+    }
+    
+    // post/POST
+    $.ajax({
+        method: 'POST',
+        url: '/owners',
+        data: ownerToSend,
+        success: function(response) {
+            console.log('succesful post response:', response);
+            //getAllPets();
+        }
+    });
+}
