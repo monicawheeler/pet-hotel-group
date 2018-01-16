4bbea07469e49dd6b15f114da30bffaabcd25728
Darian N <darian.nasrabadi@gmail.com>
2018-01-16 16:11:48 -0600

    added server to database get code

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -24,18 +24,21 @@ function getAllPets() {
 }
 
 function displayAllPets(data) {
-    $tableRow = $('<tr>')
-    $tableRow.append(`<td>${data.first_name} ${data.last_name}</td>`)
-    $tableRow.append(`<td>${data.pet_name}</td>`)
-    $tableRow.append(`<td>${data.breed}</td>`)
-    $tableRow.append(`<td>${data.color}</td>`)
-    $tableRow.append(`<td><button class="editButton" value="${data.pets_id}">IN</button></td>`)
-    $tableRow.append(`<td><button class="deleteButton" value="${data.pets_id}">IN</button></td>`)
-    if (data.is_checked_in == false) {
-        $tableRow.append(`<td><button class="checkIn" value="${data.pets_id}>IN</button></td>`)
+    console.log(data.is_checked_in);
+    $tableRow = $('<tr>');
+    $tableRow.append(`<td>${data.first_name} ${data.last_name}</td>`);
+    $tableRow.append(`<td>${data.pet_name}</td>`);
+    $tableRow.append(`<td>${data.breed}</td>`);
+    $tableRow.append(`<td>${data.color}</td>`);
+    $tableRow.append(`<td><button class="editButton" value="${data.pets_id}">Edit</button></td>`);
+    $tableRow.append(`<td><button class="deleteButton" value="${data.pets_id}">Delete</button></td>`);
+    if (data.is_checked_in === false) {
+        console.log('in the false');
+        $tableRow.append(`<td><button class="checkIn" value="${data.pets_id}">IN</button></td>`);
     }
-    else if (data.is_checked_in == true) {
-    $tableRow.append(`<td><button class="checkOut" value="${data.pets_id}>OUT</button></td>`)
+    else if (data.is_checked_in === true) {
+        console.log('in the true');
+        $tableRow.append(`<td><button class="checkOut" value="${data.pets_id}">OUT</button></td>`);
     }
     $('#tableBody').append($tableRow);
 }
