f148ab4365be638a820e4398d5b5b28766668600
Paul Tiller <pftiller@gmail.com>
2018-01-16 16:58:47 -0600

    Add register new pet post

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,13 +4,10 @@ console.log('JavaScript Sourced');
 $(document).ready(function() {
    console.log('jQuery Sourced');
    getOwnerNames();
+   //event listeners
    $('#register_new_pet').on('click', registerNewPet)
 });
-
-
-
-
-
+ 
 function getOwnerNames() {
     $.ajax({
         method: 'GET',
@@ -28,15 +25,24 @@ function displayOwnerNames(ownersArr) {
     $('#owner_name').empty();
     $('#owner_name').append('<option selected="selected" required>Your Name</option>');
     for (var i = 0; i < ownersArr.length; i++) {
-        $('#owner_name').append('<option>'+ ownersArr[i].first_name + ' ' +  ownersArr[i].first_name + '</option>');
+        $('#owner_name').append('<option data-id="' + ownersArr[i].id + '">'+ ownersArr[i].first_name + ' ' +  ownersArr[i].last_name + '</option>');
 
     }
 }
 
 function registerNewPet() {
+    
+    let newPetToSend = {
+    pet_name: $('#pet_name').val(),
+    breed: $('#breed').val(),
+    color: $('#color').val(),
+    owner_id: $('#owner_name option:selected').data('id')
+    }
+    console.log(newPetToSend);
     $.ajax({
         method: 'POST',
         url: '/pets',
+        data: newPetToSend,
         success: function(response){
           console.log('register new pet: ', response);
         }
--- server/routes/pets.router.js
+++ server/routes/pets.router.js
@@ -8,8 +8,20 @@ const pool= require('../modules/pool');
 
 
 //POST routes
+router.post('/', (req,res) => {
+   
+    const queryText = 'INSERT INTO pets (pet_name, breed, color, owner_id) VALUES ($1, $2, $3, $4)';
+    pool.query(queryText, [req.body.pet_name, req.body.breed, req.body.color, req.body.owner_id])
+        .then((result) => {
+            console.log('registed new pet');
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log('Oh no!', err);
+            res.sendStatus(500);
+        })
 
-
+});
 //PUT routes
 
 
